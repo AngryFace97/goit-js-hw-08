f5a4b0a2a0090bc4b225b19be9de89d51a4ab323
Paweł Kłodowski <angryface97@gmail.com>
2022-05-31 00:20:01 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(setLocalRecords, 500));
+
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+function setLocalRecords() {
+  const formVal = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formVal));
+}
+
+function getLocalRecords() {
+  let localObject = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (localObject === null) {
+    return;
+  }
+  email.value = localObject.email;
+  message.value = localObject.message;
+}
+
+getLocalRecords();
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  this.reset();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+}
