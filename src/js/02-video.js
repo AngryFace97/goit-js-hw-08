4d3f11ad575d81f297d42fea57b707ee3aa976ae
Paweł Kłodowski <angryface97@gmail.com>
2022-05-31 00:20:03 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on(
+  'timeupdate',
+  _.throttle(function (currentTime) {
+    seconds: currentTime;
+    let timeUpdate = Math.round(currentTime.seconds);
+
+    localStorage.setItem('videoplayer-current-time', `${timeUpdate}`);
+  }, 1000),
+);
+
+let newTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(newTime);
